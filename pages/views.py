cc442946571151e2a36957a6ff1d28271715e65c
Akhilesh <akhilesh.verma1602@gmail.com>
2023-07-06 16:18:50 +0530

    completed home page

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Team
+from cars.models import Car
 # Create your views here.
 
 def home(request):
     teams = Team.objects.all()
+    featured_cars = Car.objects.order_by('-created_data').filter(is_featured = True)
+    latest_cars = Car.objects.order_by('-created_data')
     data = {
-       'teams' : teams
+        'featured_cars' : featured_cars,
+        'latest_cars' : latest_cars,
+       'teams' : teams,
     }
     return render(request, "pages/home.html", data)
 
