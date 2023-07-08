18d92ee8c768e1b45ae78e73c78cc4787675cdf6
Akhilesh <akhilesh.verma1602@gmail.com>
2023-07-08 16:24:49 +0530

    completed search functionality

--- cars/urls.py
+++ cars/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from cars import views
 
 urlpatterns = [
-    path('', views.cars , name = "cars")
+    path('', views.cars , name = "cars"),
+    path('<int:id>', views.car_detail, name = "car_detail"),
+    path('search', views.search, name = "search")
 ]
--- pages/views.py
+++ pages/views.py
@@ -7,10 +7,22 @@ def home(request):
     teams = Team.objects.all()
     featured_cars = Car.objects.order_by('-created_data').filter(is_featured = True)
     latest_cars = Car.objects.order_by('-created_data')
+    #search_fields = Car.objects.values('model', 'city', 'year', 'body_style')
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat = True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
     data = {
         'featured_cars' : featured_cars,
         'latest_cars' : latest_cars,
        'teams' : teams,
+       #'search_fields': search_fields,
+       'model_search': model_search,
+       'city_search' : city_search,
+       'body_style_search' : body_style_search,
+       'year_search' : year_search, 
+
+
     }
     return render(request, "pages/home.html", data)
 
